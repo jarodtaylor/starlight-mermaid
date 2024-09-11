a8386ad2542e6541f7288ec944cd9b6d82f5649a
jarodtaylor <jarodrtaylor@gmail.com>
2024-09-11 12:04:37 -0500

    fixed broken mdx import

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
-import mdx from "@astro/mdx";
+import mdx from "@astrojs/mdx";
 import { rehypeMermaid } from "@beoe/rehype-mermaid";
 
 // https://astro.build/config
