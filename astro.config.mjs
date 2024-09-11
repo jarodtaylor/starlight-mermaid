c283243c13d16517d84035f0931f566af6cb1ecb
jartaylo_elcomp <jartaylo@estee.com>
2024-09-11 10:28:22 -0500

    starlight with mermaid working using .md

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,38 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import { rehypeMermaid } from "@beoe/rehype-mermaid";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: "My Docs",
+      social: {
+        github: "https://github.com/withastro/starlight",
+      },
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", slug: "guides/example" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+      customCss: ["./src/styles/custom.css"],
+    }),
+  ],
+  markdown: {
+    rehypePlugins: [
+      [
+        rehypeMermaid,
+        { class: "not-content", strategy: "inline-class-dark-mode" },
+      ],
+    ],
+  },
 });
